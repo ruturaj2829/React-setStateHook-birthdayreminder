c4bc11ac30ef579f60b2b8df032a24f005d13f97
ruturaj2829 <81922992+ruturaj2829@users.noreply.github.com>
2023-11-18 02:02:03 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Shadab khan',
+      age: 20,
+      image:
+        './profiles/shadab.jpg',
+    },
+    {
+      id: 2,
+      name: 'Shifa Naaz',
+      age: 10,
+      image:
+        './profiles/IMG-20220309-WA0014.jpg',
+    },
+    {
+      id: 3,
+      name: 'Mansi Rathod',
+      age: 20,
+      image:
+        './profiles/mansi.jpg',
+    },
+    {
+      id: 4,
+      name: 'Afrin Khan',
+      age: 20,
+      image:
+        './profiles/IMG-20211108-WA0008.jpg',
+    },
+    {
+      id: 5,
+      name: 'Arya Shikhare',
+      age: 20,
+      image:
+        './profiles/arya.jpg',
+    },
+  ];
